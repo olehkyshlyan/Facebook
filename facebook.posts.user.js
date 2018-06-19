826b1d92be8a4ba1eadacb9c6aad2576d80b6932
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2018-06-19 23:16:30 +0300

    User script for Facebook page "posts"

---
+++ facebook.posts.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Facebook | Posts
+// @namespace    https://www.facebook.com/
+// @noframes
+// @version      0.1
+// @description  Facebook | www.facebook.com | Posts
+// @author       You
+// @match        https://www.facebook.com/*/posts/*
+// @grant        none
+// ==/UserScript==
+
+//alert('Facebook | Posts');
+
+var FacebookPosts = new function(){
+  this.removeLoginBanner = function(){
+    var headerArea = document.getElementById('headerArea');
+    if(headerArea != null){
+      var hdrArFirstChild = headerArea.children[0];
+      if(hdrArFirstChild != undefined){
+        hdrArFirstChild.style.display = "none";
+      }
+    }
+  }
+
+  this.moveUp45mqClassElement = function(){
+    var moveUp45mqInclosure = function(){
+      var cls45mqCollection = document.getElementsByClassName('_45mq');
+      if(cls45mqCollection.length > 0){
+        var el45mq = cls45mqCollection[0];
+        el45mq.style.marginTop = '0px';
+      }
+
+      var u0h = document.getElementById('u_0_h');
+      u0h.style.cssText += '; position: static !important;';
+    }
+    setTimeout(moveUp45mqInclosure,2000);
+  }
+}
+
+FacebookPosts.removeLoginBanner();
+FacebookPosts.moveUp45mqClassElement();
